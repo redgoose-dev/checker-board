27a6f3338629139213eb16dcba71af061e6cc168
redgoose <scripter@me.com>
2020-11-09 19:07:54 +0900

    기초 조정중..

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
 <main>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
 </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 });
 </script>
 
-<style lang="scss">
-#app {
+<style lang="scss" scoped>
+main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Preference/index.vue
@@ -0,0 +1,19 @@
+<template>
+<article>
+  preference screen
+</article>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Preference',
+  components: {},
+  methods: {},
+});
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
